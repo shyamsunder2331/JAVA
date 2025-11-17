5b0ec8d1e92c1f9d6ebe6fe6ab24f555e331a25a
BOBBILLA SHYAMSUNDER <shyamsunder2331@gmail.com>
2025-11-17 21:19:50 +0530

    Update GapIntervals.java

--- DSA/MergeIntervsals/GapIntervals.java
+++ DSA/MergeIntervsals/GapIntervals.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-public class Main
+public class MergeIntervals
 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
