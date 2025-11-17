bcef443f3670dc75a78aacf84a1d3dc92022d9d9
BOBBILLA SHYAMSUNDER <shyamsunder2331@gmail.com>
2025-11-17 21:20:37 +0530

    Update MergeInterval.java

--- DSA/MergeIntervsals/MergeInterval.java
+++ DSA/MergeIntervsals/MergeInterval.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-public class Main
+public class MergeInterval
 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
