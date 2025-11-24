ddf1a9d1b8141a918baf4b719ab46eb21d98e171
BOBBILLA SHYAMSUNDER <shyamsunder2331@gmail.com>
2025-11-24 09:37:12 +0530

    Update DisjointSet.java

--- DSA/Graphs/MinSpanTrees/DisjointSet.java
+++ DSA/Graphs/MinSpanTrees/DisjointSet.java
@@ -23,10 +23,8 @@ class DisjointSetByRank{
         if(ulp_v==ulp_u) return;
         if(rank.get(ulp_u)<rank.get(ulp_v)){
             parent.set(ulp_u,ulp_v);
-            rank.set(ulp_v,rank.get(ulp_v)+1);
         }else if(rank.get(ulp_v)<rank.get(ulp_u)){
             parent.set(ulp_v,ulp_u);
-            rank.set(ulp_u,rank.get(ulp_u)+1);
         }else{
             parent.set(ulp_u,ulp_v);
             int rankU=rank.get(ulp_v);
